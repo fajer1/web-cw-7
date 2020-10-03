876b9921d1e6bb93e490e921fdbf962d3056dc3f
فجر 150 <fajeraljuwaisri3@gmailcom>
2020-10-03 22:04:56 +0300

    fajer

--- scripts.js
+++ scripts.js
@@ -1,23 +1,35 @@
 const counter = document.querySelector(".counterButton");
 
 // 👇🏻 مكان النقاط count قم بتعيين القيمة صفر إلى المتغير
-// ...
+let count = 0 ;
 
-counter.addEventListener("click", incrementCounter);
+counter.addEventListener("click", incrementCounter );
 
 /******************************************************
  *  هي أداة تقوم بتكرار الكود الذي incrementCounter()  *
  *  تكتبه داخلها كلما ضغطت على الزر                    *
  ******************************************************/
 
-function incrementCounter() {
+function incrementCounter( ) {
   // 👇🏻 بواحد مكان النقاط count قم بزيادة قيمة
-  // ...
+  count = count + 1 
   counter.innerHTML = count;
   // "تسوي 1 اطبع "جيد count إن كانت قيمة العداد أو
-  if (condition) {
-    document.getElementById("status").innerHTML = "جيد";
+  if ( count==1) {
+    document.getElementById("status").innerHTML = "جيد"; } 
+  else {
+    if (count >1 && count<6) {
+      document.getElementById("status").innerHTML = "جيد جدا ";} 
   }
+    
+  if (count>=6) {
+    document.getElementById("status").innerHTML = " رائع";
+  }
+ else {
+  if (count >1 && count<6) {
+    document.getElementById("status").innerHTML = "جيد جدا ";} 
+ }
+
   // "أكبر من 1 وأقل من 6 اطبع "جيد جداً count إن كانت قيمة العداد أو
   // "أكبر من أو تساوي 6 اطبع "رائع count إن كانت قيمة العداد أو
 }
